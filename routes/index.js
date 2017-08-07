3af21036661a45b23ee334c5d6a72cf1d9c64b57
choiseonho <csh8724@gmail.com>
2017-08-07 15:17:15 +0900

    database update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const MySQL = require('mysql');
+const MySQLConfig = require('../config/mysql');
+const connection = MySQL.createConnection(MySQLConfig);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.put('/add', (req, res) => {
+  connection.query('insert into token(token) values (?)', req.payload, (err, result) => {
+    connection.end();
+  });
+})
 
 module.exports = router;

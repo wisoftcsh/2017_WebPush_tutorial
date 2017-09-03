aa76f6c94723a09f286e5632981d8f0eb8c33413
choiseonho <csh8724@gmail.com>
2017-09-03 18:59:34 +0900

    update

--- config/mysql.js
+++ config/mysql.js
@@ -2,9 +2,9 @@
  * Created by choiseonho on 2017. 3. 10..
  */
 module.exports = {
-  host: 'jusk2.asuscomm.com',
-  user: 'space',
-  password: 'dblab321!',
-  database: 'space'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 };
 
